be8bd91da11578912e7989324df763c2cb9ad02d
Markus Schmidt <m.schmidt@acemail.de>
2016-02-27 17:40:05 +0100

    Updated to babel 6

--- index.ts
+++ index.ts
@@ -1,37 +1,68 @@
-export default function ({types: t}) {
-    var update;
-
+export default function({types: t}) {
     return {
         visitor: {
-            'AssignmentExpression|UpdateExpression'(path) {
+            AssignmentExpression(path, state) {
+				assignment(path, state, path.node.left);
+			},
+			
+            UpdateExpression(path, state) {
+				assignment(path, state, path.node.argument);
+			},
+
+            UnaryExpression(path, state) {
                 var node = path.node,
 					arg = node.argument;
 
+				if (node.updated || !t.isMemberExpression(arg) || node.operator !== 'delete') {
+					return;
+				}
+				
 				node.updated = true;
 				
-                if (t.isMemberExpression(arg)) {
-                    var literal = arg.computed ? arg.property : t.literal(arg.property.name);
-
-                    return t.callExpression(update, [arg.object, literal, node]);
-                } else {
-                    return t.callExpression(update, [node]);
-                }
-            },
-			
-            UnaryExpression(path) {
-                var node = path.node,
-					arg = node.argument;
-
-                if (t.isMemberExpression(arg) && node.operator === 'delete') {
-                    var literal = arg.computed ? arg.property : t.literal(arg.property.name);
-
-                    return t.callExpression(update, [arg.object, literal]);
-                }
-            },
-			
-            Program(path) {
-                update = path.file.addImport('access-core/observer/update');
+				path.replaceWith(
+					t.callExpression(
+						update(state),
+						[
+							arg.object,
+							arg.computed ? arg.property : t.stringLiteral(arg.property.name)
+						]
+					)
+				);
             }
         }
     };
+	
+	function assignment(path, state, arg) {
+		var node = path.node;
+
+		if (node.updated) {
+			return;
+		}
+		
+		node.updated = true;
+		
+		var params = [node];
+
+		if (t.isMemberExpression(arg)) {
+			if (t.isIdentifier(arg.object) && arg.object.name === 'exports') {
+				return;
+			}
+			
+			params.unshift(
+				arg.object,
+				arg.computed ? arg.property : t.stringLiteral(arg.property.name)
+			);
+		}
+		
+		path.replaceWith(
+			t.callExpression(
+				update(state),
+				params
+			)
+		);
+	}
+}
+
+function update(state) {
+	return state.addImport('access-core/observer/update', 'default');
 }
