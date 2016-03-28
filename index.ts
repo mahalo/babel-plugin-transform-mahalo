5b72a88a20510c68b30b2bc46a17751b0790e45e
Markus Schmidt <m.schmidt@acemail.de>
2016-03-28 22:10:30 +0200

    Added support for ES2015 Array and String instance methods

--- index.ts
+++ index.ts
@@ -1,7 +1,25 @@
-var MAHALO = /^(node_modules|\.\.)\\mahalo\\/;
+var MAHALO = /^(node_modules|\.\.)\\mahalo\\/,
+	METHODS = [
+		// Array
+		'copyWithin',
+		'entries',
+		'fill',
+		'find',
+		'findIndex',
+		'keys',
+		'values',
+		
+		// String
+		'codePointAt',
+		'endsWith',
+		'includes',
+		'repeat',
+		'startsWith'
+	];
 
 export default function({types: t}) {
-	var skip;
+	var first = true,
+		skip;
 	
     return {
 		pre(file) {
@@ -68,7 +86,44 @@ export default function({types: t}) {
 						]
 					)
 				);
-            }
+            },
+			
+			CallExpression(path, state) {
+				var node = path.node,
+					callee = node.callee,
+					property;
+				
+				if (!t.isMemberExpression(callee)) {
+					return;
+				}
+				
+				property = callee.property;
+				
+				if (callee.computed || METHODS.indexOf(property.name) < 0) {
+					return;
+				}
+				
+				path.replaceWith(
+					t.callExpression(
+						callMethod(state),
+						[
+							callee.object,
+							t.stringLiteral(property.name),
+							t.arrayExpression(node.arguments)
+						]
+					)
+				);
+			},
+			
+			Program: {
+				exit(path, state) {
+					if (first) {
+						state.addImport('mahalo/polyfill/set-prototype-of', null, null);
+						state.addImport('mahalo/polyfill/get-prototype-of', null, null);
+						first = false;
+					}
+				}
+			}
         }
     };
 	
@@ -102,4 +157,8 @@ export default function({types: t}) {
 
 function assign(state) {
 	return state.addImport('mahalo/change-detection/property', 'assign', 'assign');
+}
+
+function callMethod(state) {
+	return state.addImport('mahalo/polyfill/call-method', 'default', 'callMethod');
 }
